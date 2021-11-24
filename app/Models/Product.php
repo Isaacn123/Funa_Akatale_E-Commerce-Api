1d73df9ba6921d596d8f158772db58b1110e6268
isaac1212 <nsambai722@gmail.com>
2021-11-25 02:39:07 +0300

    Relationship created for both Products and Reviews

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+
+
+    public function reviews(){
+
+        return $this->hasMany('App\Models\Review');
+    }
 }
